3966d9e991cd5c9fe108cd1b8640c7341aff766d
wmfsystem <willianmarquesfreire@gmail.com>
2018-12-07 18:18:32 -0200

    Distribuição normal

--- statistic.js
+++ statistic.js
@@ -211,3 +211,170 @@ class Variabilidade {
 // console.log(variabilidade.variancia());
 // console.log(variabilidade.desvioPadrao());
 // console.log(variabilidade.amplitude());
+
+/*
+    ** Probabilidade (P): 0 <= P <= 1
+        - P = 1: Evento certo
+        - P = 0: Evento impossível
+        - Prob. 50%: 0.5 ou 1/2
+        - Impossível: -.0.5 ou -20% ou 2/1
+    - Experimento: O que está sendo estudado - jogar moeda
+    - Espaço amostral: Todas possíbilidades de ocorrências do evento - cara ou coroa
+    - Evento: resultados ocorridos - coroa
+
+    - Eventos Excludentes: Não podem ocorrer ao mesmo tempo - jogar um dado e ser 1 e par
+    - Eventos Não Excludentes: Quando podem ocorrer ao mesmo tempo - jogar um dado e ser 2 e par
+    - Eventos Dependentes: A ocorrência de um evento afeta o outro. Um ocorre depois do outro ocorrer
+    - Eventos Independentes: A ocorrência de um evento não afeta o outro
+
+    - P = Ocorrência Esperada / Número de Eventos Possíveis
+        - Exemplos
+            - Jogar moeda e dar cara: P = 1/2 ou 0.5 ou 50%
+            - Jogar dado e dar 6: P = 1/6 ou 0.16 ou 16%
+            - Jogar dado e dar 1 OU 6: P = 2/6, P=0.33
+            - Jogar dado e dar 1,2,3,4,5 ou 6: P = 6/6 ou 1 ou 100%
+            - Jogar dado e dar ímpar ou maior que 4 {ímpar: (1,3,5); n > 4: (5,6)} = (1,3,5,6): P = 4/6 ou 0.66 ou 66%
+        
+        - Eventos Excludentes: Soma das probabilidades
+            - Jogar dado e ser 1 ou par: 1/6 + 3/6 = 4/6
+        - Eventos Não Excludentes:
+            - Jogar um dado e ser 2 ou par: 1/6 + 3/6 - 1/6 = 3/6
+        - Eventos Independentes: Multiplica as probabilidades
+            - Jogar dois dados, e dar 1 e 6: 1/6 * 1/6 = 1/36
+        - Eventos Dependentes: 
+            - Dada 6 cartas (A,2,3,4,5,6), qual a probabilidade de no primeiro evento tirar A e no segundo 4 (Não há reposição, então no primeiro evento n = 6 e no segundo n = 5)
+                1/6 * 1/5 = 1/30
+        -
+ */
+
+
+class Probabilidade {
+    constructor(ocorrenciaEsperada, eventosPossiveis) {
+        this.ocorrenciaEsperada = ocorrenciaEsperada;
+        this.eventosPossiveis = eventosPossiveis;
+    }
+    excludente() {
+        return this.ocorrenciaEsperada.reduce((a, b) => a + b, 0) / this.eventosPossiveis;
+    }
+    naoExcludente() {
+        return (this.ocorrenciaEsperada.reduce((a, b) => a + b, 0) - this.ocorrenciaEsperada[0]) / this.eventosPossiveis;
+    }
+    dependente() {
+        let aux = 0;
+        return this.ocorrenciaEsperada.reduce((a, b) => {
+            let result = a * b / (this.eventosPossiveis - aux);
+            aux = a;
+            return result;
+        }, 1);
+    }
+    independente() {
+        return this.ocorrenciaEsperada.reduce((a, b) => a * b / this.eventosPossiveis, 1);
+    }
+}
+
+
+// console.log(new Probabilidade([1, 3], 6).excludente())
+// console.log(new Probabilidade([1, 3], 6).naoExcludente())
+// console.log(new Probabilidade([1, 1], 6).independente())
+// console.log(new Probabilidade([1, 1], 6).dependente())
+
+/*
+    ** Distribuição **
+    - Verificar comportamento de dados aleatórios
+
+    - Distribuição Binomial
+        - Distribuição de Probabilidade Discreta
+        - Pré-requisitos:
+            - Número fixo de experimentos
+            - Cada experimento pode ter sucesso ou fracasso como resultado
+            - A probabilidade de sucesso deve ser a mesma em cada experimento
+            - Os experimentos são independentes
+        - Convenções:
+            - X: Total de sucesso esperado do experimento
+            - p: Probabilidade de sucesso
+            - n: número de experimentos
+            - 1-p: Probabilidade de fracasso
+
+        Fórmula: Fatorial(n x) * p ^ x * (1-p) ^ (n-x)
+        Ex: Se jogar moeda 5 vezes. Qual probabilidade de dar cara 3 vezes:
+            - X = 3; p = 0.5; n = 5
+
+    - Distribuição Normal
+        - Teorema Central do Limite: Conforme o tamanho da amostra aumenta,
+            a distribuição das médias amostrais se aproximam cada vez mais da 
+            distribuição normal
+        - Independente de como os dados estão distribuídos, suas médias estão
+            normalmente distribuídas
+        - Distribuição Normal Padrão (Z)
+            - Mostra o número de desvios padrões que o valor está acima ou
+                abaixo da média (score z ou valor z)
+            - Média Zero
+            - Deviso Padrão 1
+            - Usa-se a fórmula para calcular a probabilidade dos dados em relação
+                a tabela Z. Z = (amostra - média) / desvioPadrão
+*/
+
+class DistribuicaoBinomial {
+    // sucessoEsperado, probabilidadeSucesso, nrExperimentos
+    constructor(x, p, n) {
+        this.x = x;
+        this.p = p;
+        this.n = n;
+    }
+    calcula() {
+        let fatorial = this.fatorial(this.n)
+            / (this.fatorial(this.x) * this.fatorial(this.n - this.x));
+        return fatorial * Math.pow(this.p, this.x) * Math.pow(1 - this.p, this.n - this.x)
+    }
+    cumulativa() {
+        let soma = 0;
+        while (this.x >= 0) {
+            soma += this.calcula();
+            this.x--;
+        }
+        return soma;
+    }
+    fatorial(n) {
+        if (n <= 0) return 1;
+        return n * this.fatorial(n - 1);
+    }
+}
+// console.log(new DistribuicaoBinomial(3, 0.5, 5).calcula())
+// console.log(new DistribuicaoBinomial(0, 0.25, 4).calcula())
+// console.log(new DistribuicaoBinomial(1, 0.25, 4).calcula())
+// console.log(new DistribuicaoBinomial(2, 0.25, 4).calcula())
+// console.log(new DistribuicaoBinomial(3, 0.25, 4).calcula())
+// console.log(new DistribuicaoBinomial(4, 0.25, 4).calcula())
+// console.log(new DistribuicaoBinomial(4, 0.25, 4, true).cumulativa())
+
+
+class DistribuicaoNormal {
+    // sucessoEsperado, média, desvioPadrão
+    constructor(x, u, o) {
+        this.x = x;
+        this.u = u;
+        this.o = o;
+    }
+    z() {
+        let z = (this.x - this.u) / this.o;
+        return z;
+    }
+
+    normalcdf(X) {
+        var T = 1 / (1 + .2316419 * Math.abs(X));
+        var D = .3989423 * Math.exp(-X * X / 2);
+        var Prob = D * T * (.3193815 + T * (-.3565638 + T * (1.781478 + T * (-1.821256 + T * 1.330274))));
+        if (X > 0) {
+            Prob = 1 - Prob
+        }
+        return Prob
+    }
+
+    calcula() {
+        return this.normalcdf(this.z())
+    };
+
+}
+
+// console.log(new DistribuicaoNormal(6, 8, 2).calcula())
+
