b738ad2bd5273e191c3c8da070e8526aeadf9885
wmfsystem <willianmarquesfreire@gmail.com>
2018-12-13 22:02:20 -0200

    T Studen

--- statistic.js
+++ statistic.js
@@ -533,3 +533,85 @@ class TesteDeHipotese {
 // console.log(new TesteDeHipotese(100, 23).estatisticaTesteMedia(4, 22))
 // Em média 75% votam em A; H0 = p = 0,75; Ha = p < 0,75
 // console.log(new TesteDeHipotese(100, 73).estatisticaTesteProporcao(75))
+
+/*
+    ** T de Student **
+    - Utilizada quando a amostra é pequena n < 30, e não se conhece o desvio padrão
+    - Possui como custo maior variabilidade
+    - Tendência maior de encontrar valores nas caudas
+    - Se n >= 30, se assemelha a distribuição normal
+    - Grau de Liberdade: corresponde ao tamanho da amostra (n-1)
+
+    - Pode-se usar T de Student para: calcular probabilidades, intervalos de 
+    confiança e executar testes de hipótese
+
+    - http://www.math.ucla.edu/~tom/distributions/
+    - Calcula valor t, consulta a tabela de distribuição t
+
+    - t = (X - média) / (S / raizQuadrada de n)
+*/
+
+class TStudent {
+    // Média da população, desvio padrão da amostra, tamanho da amostra, graus de liberdade
+    constructor(u0, s, n) {
+        this.u0 = u0;
+        this.s = s;
+        this.n = n;
+        this.t0 = n - 1;
+    }
+    calcula(x) {
+        this.t = (x - this.u0) / (this.s / Math.sqrt(this.n));
+        return this.compute(this.t, this.t0)
+    }
+    logGamma(Z) {
+        var S = 1 + 76.18009173 / Z - 86.50532033 / (Z + 1) + 24.01409822 / (Z + 2) - 1.231739516 / (Z + 3) + .00120858003 / (Z + 4) - .00000536382 / (Z + 5);
+        var LG = (Z - .5) * Math.log(Z + 4.5) - (Z + 4.5) + Math.log(S * 2.50662827465);
+        return LG
+    }
+    compute(X, df) {
+        let betacdf = 0;
+        let tcdf = 0;
+        let A = df / 2;
+        let S = A + .5;
+        let Z = df / (df + X * X);
+        let BT = Math.exp(this.logGamma(S) - this.logGamma(.5) - this.logGamma(A) + A * Math.log(Z) + .5 * Math.log(1 - Z));
+        if (Z < (A + 1) / (S + 2)) {
+            betacdf = BT * this.betinc(Z, A, .5)
+        } else {
+            betacdf = 1 - BT * this.betinc(1 - Z, .5, A)
+        }
+        if (X < 0) {
+            tcdf = betacdf / 2
+        } else {
+            tcdf = 1 - betacdf / 2
+        }
+        return Math.round(tcdf * 100000) / 100000;
+    }
+    betinc(X, A, B) {
+        var A0 = 0;
+        var B0 = 1;
+        var A1 = 1;
+        var B1 = 1;
+        var M9 = 0;
+        var A2 = 0;
+        var C9;
+        while (Math.abs((A1 - A2) / A1) > .00001) {
+            A2 = A1;
+            C9 = -(A + M9) * (A + B + M9) * X / (A + 2 * M9) / (A + 2 * M9 + 1);
+            A0 = A1 + C9 * A0;
+            B0 = B1 + C9 * B0;
+            M9 = M9 + 1;
+            C9 = M9 * (B - M9) * X / (A + 2 * M9 - 1) / (A + 2 * M9);
+            A1 = A0 + C9 * A1;
+            B1 = B0 + C9 * B1;
+            A0 = A0 / B1;
+            B0 = B0 / B1;
+            A1 = A1 / B1;
+            B1 = 1;
+        }
+        return A1 / A
+    }
+    // Olhar na tabela T de Student
+}
+
+// console.log(new TStudent(75, 10, 9).calcula(80))
