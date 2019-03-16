2d6dc2edc4a4eb02dd4a69c74f37c9f524485e08
willianmarquesfreire <willianmarquesfreire@gmail.com>
2019-03-16 18:55:13 -0300

    mlp

--- mlp.js
+++ mlp.js
@@ -178,7 +178,7 @@ class Neuronio {
     }
 }
 
-class Backpropagation {
+class MLP {
     constructor(attrs) {
         this.dados = attrs.dados;
         this.epocas = attrs.epocas;
@@ -271,100 +271,9 @@ class Backpropagation {
 }
 
 module.exports = {
-    Backpropagation: Backpropagation,
-    Neuronio: Neuronio
+    MLP: MLP,
+    Neuronio: Neuronio,
+    FuncaoAtivacao: FuncaoAtivacao,
+    CamadaDensa: CamadaDensa,
+    Peso: Peso
 }
-
-let funcaoAtivacao = new FuncaoAtivacao('sigmoid');
-
-let dados = [
-    [0, 0, 1],
-    [0, 1, 1],
-    [1, 0, 1],
-    [1, 1, 1]
-];
-
-console.log("-----------> Binário")
-// Probabilidade, binário
-
-let desejado = [0, 1, 1, 0]
-
-let entrada = new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao});
-let oculta = new CamadaDensa({ pesos: [[0], [0], [0]], funcaoAtivacao: funcaoAtivacao});
-let saida = new CamadaDensa({ pesos: [0], funcaoAtivacao: funcaoAtivacao});
-
-let bp = new Backpropagation({
-    camadas: [entrada, oculta, saida],
-    txaprendizagem: 0.3,
-    dados: dados,
-    epocas: 10000,
-    showLogs: false,
-    desejado: desejado
-});
-
-bp.treina();
-
-dados.forEach(dado => {
-    console.log(bp.prediz(dado).maxValue)
-})
-
-console.log("-----------> MultiClasse")
-// Probabilidade, multiclasse
-
-dados = [
-    [0, 0, 0, 0, 1],
-    [0, 0, 1, 1, 1],
-    [0, 1, 0, 1, 1],
-    [0, 1, 1, 1, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1],
-    [1, 0, 0, 1, 1],
-    [1, 1, 0, 0, 1],
-    [1, 1, 1, 1, 1],
-    [2, 0, 0, 1, 1],
-    [2, 0, 1, 1, 1],
-    [2, 0, 1, 1, 1],
-    [2, 0, 0, 0, 1],
-    [2, 1, 1, 1, 1],
-    [2, 1, 0, 1, 1],
-];
-// nenhuma, gelatinosa, dura
-// desejado = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
-desejado = [
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 1, 0],
-    [1, 0, 0],
-    [0, 0, 1],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 1, 0],
-    [0, 0, 1],
-    [0, 1, 0],
-    [1, 0, 0],
-    [0, 1, 0],
-    [0, 1, 0]
-];
-
-
-bp = new Backpropagation({
-    txaprendizagem: 0.3,
-    dados: dados,
-    camadas: [
-        new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao}),
-        new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao}),
-        new CamadaDensa({ pesos: [0, 0, 0], funcaoAtivacao: funcaoAtivacao})
-    ],
-    epocas: 1000,
-    showLogs: false,
-    desejado: desejado
-});
-
-bp.treina();
-
-dados.forEach((dado, i) => {
-    let obt = bp.prediz(dado);
-    console.log("Esperado: " + desejado[i].reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0) + ", Obtido: " + obt.maxIndex)
-})
---
+++ mlpTest.js
@@ -0,0 +1,98 @@
+let m = require("./mlp");
+let MLP = m.MLP;
+let FuncaoAtivacao = m.FuncaoAtivacao;
+let Neuronio = m.Neuronio;
+let CamadaDensa = m.CamadaDensa;
+
+let funcaoAtivacao = new FuncaoAtivacao('sigmoid');
+
+let dados = [
+    [0, 0, 1],
+    [0, 1, 1],
+    [1, 0, 1],
+    [1, 1, 1]
+];
+
+console.log("-----------> Binário")
+// Probabilidade, binário
+
+let desejado = [0, 1, 1, 0]
+
+let entrada = new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao});
+let oculta = new CamadaDensa({ pesos: [[0], [0], [0]], funcaoAtivacao: funcaoAtivacao});
+let saida = new CamadaDensa({ pesos: [0], funcaoAtivacao: funcaoAtivacao});
+
+let bp = new MLP({
+    camadas: [entrada, oculta, saida],
+    txaprendizagem: 0.3,
+    dados: dados,
+    epocas: 10000,
+    showLogs: false,
+    desejado: desejado
+});
+
+bp.treina();
+
+dados.forEach(dado => {
+    console.log(bp.prediz(dado).maxValue)
+})
+
+console.log("-----------> MultiClasse")
+// Probabilidade, multiclasse
+
+dados = [
+    [0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1],
+    [0, 1, 0, 1, 1],
+    [0, 1, 1, 1, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1],
+    [1, 0, 0, 1, 1],
+    [1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1],
+    [2, 0, 0, 1, 1],
+    [2, 0, 1, 1, 1],
+    [2, 0, 1, 1, 1],
+    [2, 0, 0, 0, 1],
+    [2, 1, 1, 1, 1],
+    [2, 1, 0, 1, 1],
+];
+// nenhuma, gelatinosa, dura
+// desejado = [[1, 0, 0], [0, 1, 0], [0, 0, 1]];
+desejado = [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [0, 0, 1],
+    [0, 1, 0],
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 1, 0]
+];
+
+bp = new MLP({
+    txaprendizagem: 0.3,
+    dados: dados,
+    camadas: [
+        new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao}),
+        new CamadaDensa({ pesos: [[0, 0, 0], [0, 0, 0], [0, 0, 0]], funcaoAtivacao: funcaoAtivacao}),
+        new CamadaDensa({ pesos: [0, 0, 0], funcaoAtivacao: funcaoAtivacao})
+    ],
+    epocas: 1000,
+    showLogs: false,
+    desejado: desejado
+});
+
+bp.treina();
+
+dados.forEach((dado, i) => {
+    let obt = bp.prediz(dado);
+    console.log("Esperado: " + desejado[i].reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0) + ", Obtido: " + obt.maxIndex)
+})
